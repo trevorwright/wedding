3b342c55da55ad4c20605c3c9fb21b287f8ac562
Trevor Wright <trevor.wright@prodigygame.com>
2018-08-30 20:34:41 -0400

    Adds our story section pending content

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,44 +1,12 @@
 import React from 'react'
 import Nav from './Nav'
 import Hero from './Hero'
+import Story from './Story'
 
 export default () => (
   <div>
     <Nav />
     <Hero />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipiscing elit cras sed, montes ut porta fringilla
-      senectus fermentum turpis nibh, pharetra a nostra libero himenaeos bibendum tristique nullam.
-      Nascetur molestie ornare dignissim fringilla sem, elit velit dui aenean, massa aptent non
-      pharetra. Nostra torquent senectus magnis porta viverra gravida hendrerit dictum, aliquet
-      phasellus proin tincidunt nam suscipit suspendisse mollis sem, aenean elementum vel velit
-      ligula potenti ridiculus. Donec consectetur interdum curae sagittis tempor dapibus
-      sollicitudin, mollis fames libero urna nisl facilisis imperdiet fringilla, nostra purus
-      curabitur ligula vestibulum porta. Conubia ipsum placerat iaculis libero est hac pharetra
-      quis, fermentum facilisi nisl sollicitudin rutrum ridiculus ultrices. Vitae consectetur cum
-      facilisis aliquet risus lacus facilisi, ipsum libero sit maecenas nec viverra consequat,
-      luctus volutpat praesent curae ornare quisque. Ridiculus amet potenti orci habitant nam vel
-      est fermentum erat primis, risus hendrerit scelerisque ut venenatis class magnis condimentum
-      nibh, massa aliquam commodo curabitur varius quisque semper sollicitudin fusce. Sed pulvinar
-      convallis adipiscing magnis ipsum montes tincidunt nunc lectus euismod, inceptos mus leo massa
-      ultricies auctor laoreet congue himenaeos sollicitudin phasellus, nulla duis vitae taciti
-      sodales elit facilisis ante posuere. Consectetur nostra proin lacus odio tempor id,
-      pellentesque turpis vehicula tempus facilisi convallis conubia, commodo quis nibh orci
-      maecenas. Nostra libero mauris lacus varius placerat habitasse fringilla gravida mi semper,
-      porttitor dictumst nec montes ultrices ut imperdiet adipiscing. Sollicitudin nascetur dolor
-      porttitor egestas duis ultrices suscipit fermentum facilisi aenean, magnis nostra quisque nunc
-      commodo hendrerit aptent morbi vestibulum, eros massa placerat pretium per dapibus dis risus
-      viverra. Dapibus proin etiam torquent montes euismod porttitor pellentesque laoreet vivamus
-      vestibulum, faucibus integer maecenas sem scelerisque sed senectus class aliquam metus
-      condimentum, eleifend molestie aptent suscipit a quam enim dis elementum. Pellentesque
-      tristique justo quis suscipit nam ultricies non, aliquam lorem egestas dictumst dignissim diam
-      gravida, sed ornare libero proin fermentum ac. Etiam ante erat habitasse dictumst eget congue
-      hac luctus donec molestie, nostra pharetra primis senectus augue ultrices orci suscipit
-      inceptos, felis ac faucibus nullam at elementum fusce mattis phasellus. Curae torquent
-      inceptos ipsum sagittis diam class rutrum nulla pulvinar, eget mollis nisi sociosqu proin
-      dapibus ad bibendum, nullam fames montes duis parturient ut adipiscing molestie. Commodo
-      primis adipiscing proin aptent vehicula scelerisque at, facilisis sociis pulvinar etiam nec
-      erat.
-    </p>
+    <Story />
   </div>
 )
