9094262dbb2c01150de2dec6bc1b49783a87cbbd
Trevor Wright <trevor.wright@prodigygame.com>
2018-11-17 13:33:46 -0500

    Creates a shared section component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Section } from './Section'
 export { default as SectionTitle } from './SectionTitle'
 export { default as Button } from './Button'
 export { default as Hero } from './Hero'
--- src/containers/Home/Story.js
+++ src/containers/Home/Story.js
@@ -2,22 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { media } from '../../styles'
 import ourStoryImage from '../../../assets/story.jpg'
-import { SectionTitle } from '../../components'
-
-const Container = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 24px;
-  text-align: center;
-  font-weight: 300;
-
-  box-shadow: inset 0px 5px 10px -1px rgba(0, 0, 0, 0.3);
-
-  ${media.medium`
-    padding: 80px 40px;
-  `};
-`
+import { Section } from '../../components'
 
 const Image = styled.img`
   display: none;
@@ -43,20 +28,6 @@ const Image = styled.img`
   `};
 `
 
-const ContentContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 900px;
-  margin: 0 auto;
-  font-size: 16px;
-
-  ${media.medium`
-    flex-direction: row;
-    align-items: flex-start;
-  `};
-`
-
 const OurStory = styled.div`
   padding: 0 40px;
   text-align: left;
@@ -71,36 +42,32 @@ const OurStory = styled.div`
 `
 
 const Story = () => (
-  <Container name="story">
-    <SectionTitle>Our Story</SectionTitle>
-    <ContentContainer>
-      <Image src={ourStoryImage} alt="" />
-      <OurStory>
-        <p>He was the grocery clerk. She was the cashier. You know how it goes.</p>
-        <p>
-          It turns out Metro is a pretty romantic place. Just kidding. But it is where it all
-          started. Over one of our shared breaks in the Metro breakroom (which Katelyn thought
-          nothing of, and Trevor planned very carefully), we found out he was also the boy next
-          door. Much more romantic. We’ll stick with that from now on.
-        </p>
-        <p>
-          After a few months of “just friends”, it all began in September 2012. It has been just
-          over 6 years, and with every day that passes (and road trip, sushi date, and cat selfie),
-          we feel more confident that we belong together. You might even say that we are Wright
-          together.
-        </p>
-        <p>
-          Trevor proposed on May 25, 2018. He somehow found the perfect mix between an intimate,
-          romantic proposal and knowing that Kate would want to share the news in person, and
-          celebrate one of the most exciting days of their lives with their family and closest
-          friends. The result was one incredible surprise during what Katelyn thought was a casual
-          hike, one fake party to celebrate her “finishing her first year of graduate school”, MANY
-          confused friends and family, and eventually, lots of tears and champagne. It was perfect.
-        </p>
-        <p>Theme: “I had no idea.” - Kate</p>
-      </OurStory>
-    </ContentContainer>
-  </Container>
+  <Section name="story" title="Our Story">
+    <Image src={ourStoryImage} alt="" />
+    <OurStory>
+      <p>He was the grocery clerk. She was the cashier. You know how it goes.</p>
+      <p>
+        It turns out Metro is a pretty romantic place. Just kidding. But it is where it all started.
+        Over one of our shared breaks in the Metro breakroom (which Katelyn thought nothing of, and
+        Trevor planned very carefully), we found out he was also the boy next door. Much more
+        romantic. We’ll stick with that from now on.
+      </p>
+      <p>
+        After a few months of “just friends”, it all began in September 2012. It has been just over
+        6 years, and with every day that passes (and road trip, sushi date, and cat selfie), we feel
+        more confident that we belong together. You might even say that we are Wright together.
+      </p>
+      <p>
+        Trevor proposed on May 25, 2018. He somehow found the perfect mix between an intimate,
+        romantic proposal and knowing that Kate would want to share the news in person, and
+        celebrate one of the most exciting days of their lives with their family and closest
+        friends. The result was one incredible surprise during what Katelyn thought was a casual
+        hike, one fake party to celebrate her “finishing her first year of graduate school”, MANY
+        confused friends and family, and eventually, lots of tears and champagne. It was perfect.
+      </p>
+      <p>Theme: “I had no idea.” - Kate</p>
+    </OurStory>
+  </Section>
 )
 
 export default Story
