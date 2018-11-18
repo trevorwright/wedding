f5e5f1b0ac212b785dd78df50485d2b63b884537
Trevor Wright <trevor.wright@prodigygame.com>
2018-11-18 10:23:24 -0500

    Updates naming convention

---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageGallery = () => (
+  <div>
+    <div>Image Gallery</div>
+  </div>
+)
+
+export default ImageGallery
--- src/containers/EngagementPhotos/EngagementPhotos.js
+++ src/containers/EngagementPhotos/EngagementPhotos.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { NavigationBar } from '../../components'
+import { NavigationBar, ImageGallery, Hero } from '../../components'
 import navItems from './nav-items'
+
+import photographyImage from '../../../assets/photography.jpg'
 
 const EngagementPhotos = () => (
   <main>
     <NavigationBar items={navItems} />
+    <Hero image={photographyImage}>Engagement Photos</Hero>
+    <ImageGallery />
   </main>
 )
 
