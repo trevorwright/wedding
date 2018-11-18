6abc9917c1e78029dfe4a2d586d23cfdbcdfacc5
Trevor Wright <trevor.wright@prodigygame.com>
2018-11-18 09:41:57 -0500

    Adds nav bar to engagement gallery

--- src/containers/EngagementPhotos/EngagementPhotos.js
+++ src/containers/EngagementPhotos/EngagementPhotos.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import { NavigationBar } from '../../components'
+import navItems from './nav-items'
 
-const EngagementPhotos = () => <div>got those photos here</div>
+const EngagementPhotos = () => (
+  <main>
+    <NavigationBar items={navItems} />
+  </main>
+)
 
 export default EngagementPhotos
---
+++ src/containers/EngagementPhotos/nav-items.js
@@ -0,0 +1,1 @@
+export default [{ text: 'Event Details', target: 'home' }]
