0a4785680cce975e673a207263674317698912f0
Trevor Wright <trevor.wright@prodigygame.com>
2018-11-18 09:27:35 -0500

    Adds 404 Page

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
+import sleepingImage from '../../assets/sleeping.jpg'
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #e1f5fe;
+
+  > *:not(:last-child) {
+    margin-bottom: 40px;
+  }
+`
+
+const ErrorMessage = styled.h1`
+  font-size: 48px;
+`
+
+const Image = styled.img`
+  height: 60vh;
+  border-radius: 5px;
+  box-shadow: 6px 6px 15px -6px rgba(0, 0, 0, 0.75);
+`
 
 export default () => (
-  <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
-  </div>
-);
+  <Container>
+    <ErrorMessage>We couldn't find that page :(</ErrorMessage>
+    <Image src={sleepingImage} alt="Kate KO'd" />
+  </Container>
+)
