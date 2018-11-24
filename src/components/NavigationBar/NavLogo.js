a77e9d0ae9d87a7ef264b9159dfc881dbbb82621
Trevor Wright <trevor.wright@prodigygame.com>
2018-11-24 10:45:04 -0500

    Logo scrolls to top of page

--- src/components/NavigationBar/NavLogo.js
+++ src/components/NavigationBar/NavLogo.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import { animateScroll } from 'react-scroll'
 
-const InitialsLogo = styled.div`
+const InitialsLogo = styled.button`
   font-size: 22px;
   font-weight: 300;
   letter-spacing: 8px;
   color: #fff;
+  cursor: pointer;
 `
 
-const NavLogo = () => <InitialsLogo>K & T</InitialsLogo>
+const onLogoClick = () => {
+  animateScroll.scrollToTop()
+}
+
+const NavLogo = () => <InitialsLogo onClick={onLogoClick}>K & T</InitialsLogo>
 
 export default NavLogo
